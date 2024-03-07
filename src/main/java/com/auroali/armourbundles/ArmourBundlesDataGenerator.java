eb5b819ef244740f40347ef5dcb440f0d86f4827
Auroali <dawn.auroali@gmail.com>
2024-03-06 20:14:03 -0400

    forgot to push this

--- src/main/java/com/auroali/armourbundles/ArmourBundlesDataGenerator.java
+++ src/main/java/com/auroali/armourbundles/ArmourBundlesDataGenerator.java
@@ -4,15 +4,22 @@ import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricLanguageProvider;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.minecraft.data.client.BlockStateModelGenerator;
 import net.minecraft.data.client.ItemModelGenerator;
 import net.minecraft.data.client.Models;
+import net.minecraft.data.server.recipe.RecipeJsonProvider;
+import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
+import net.minecraft.item.Items;
+
+import java.util.function.Consumer;
 
 public class ArmourBundlesDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 		fabricDataGenerator.addProvider(new APLangGen(fabricDataGenerator));
 		fabricDataGenerator.addProvider(new APModelGen(fabricDataGenerator));
+		fabricDataGenerator.addProvider(new APRecipeGenerator(fabricDataGenerator));
 	}
 
 	public static class APLangGen extends FabricLanguageProvider {
@@ -45,6 +52,25 @@ public class ArmourBundlesDataGenerator implements DataGeneratorEntrypoint {
 		@Override
 		public void generateItemModels(ItemModelGenerator itemModelGenerator) {
 			itemModelGenerator.register(ArmourBundles.ARMOUR_BUNDLE, Models.GENERATED);
+		}
+	}
+
+	public static class APRecipeGenerator extends FabricRecipeProvider {
+		public APRecipeGenerator(FabricDataGenerator dataGenerator) {
+			super(dataGenerator);
+		}
+
+		@Override
+		public void generateRecipes(Consumer<RecipeJsonProvider> exporter) {
+			ShapedRecipeJsonBuilder.create(ArmourBundles.ARMOUR_BUNDLE)
+					.criterion(hasItem(Items.NETHERITE_INGOT), conditionsFromItem(Items.NETHERITE_INGOT))
+					.criterion(hasItem(Items.RABBIT_HIDE), conditionsFromItem(Items.RABBIT_HIDE))
+					.pattern(" N ")
+					.pattern("R R")
+					.pattern(" R ")
+					.input('R', Items.RABBIT_HIDE)
+					.input('N', Items.NETHERITE_INGOT)
+					.offerTo(exporter);
 		}
 	}
 }
